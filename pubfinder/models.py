f5dee6cc37436e6ed3c1f0301b5c9f2c5ed0c2e0
dpfinch <d.finxch@ed.ac.uk>
2019-04-06 14:20:09 +0100

    Added pub model and admin

--- pubfinder/models.py
+++ pubfinder/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class pub_info(models.Model):
+
+    pub_name = models.CharField(max_length = 150)
+    pub_lat = models.FloatField()
+    pub_lon = models.FloatField()
+    pub_street = models.CharField(max_length = 150)
+    #  Need some attributes to the pub
+    
+
+    def __str__(self):
+        return self.pub_name
